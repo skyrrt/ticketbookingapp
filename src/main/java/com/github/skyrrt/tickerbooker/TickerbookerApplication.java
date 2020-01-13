9d49c9cb564f85823e1b7a4a1f2754fc59889c98
Bartek <bart.rzyszkiewicz@gmail.com>
2020-01-14 00:36:39 +0100

    init

---
+++ src/main/java/com/github/skyrrt/tickerbooker/TickerbookerApplication.java
@@ -0,0 +1,13 @@
+package com.github.skyrrt.tickerbooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickerbookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickerbookerApplication.class, args);
+	}
+
+}
